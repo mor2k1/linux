32d77102883474862a6b63d51a82c3c24c06ce54
TheH4ckingCollector <91323245+TheH4ckingCollector@users.noreply.github.com>
2022-07-17 11:43:24 +0300

    Java project IntelliJ commit

---
+++ src/OOP/Car.java
@@ -0,0 +1,54 @@
+package OOP;
+
+import java.util.Locale;
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model){
+        String validModel = model.toLowerCase();
+        if(validModel.equals("aaoris") || validModel.equals("corona")){
+            this.model = model;
+        }else {
+            this.model = "Unknown";
+        }
+    }
+    public String getModel(){
+        return this.model;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/OOP/Main.java
@@ -0,0 +1,10 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args){
+        Car toyota = new Car();
+        toyota.setModel("Aaoris");
+        System.out.println("Model is: " + toyota.getModel());
+
+    }
+}
