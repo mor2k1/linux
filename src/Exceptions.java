64e471c1ff2f9e42eaf66e2020d06812e4b80596
TheH4ckingCollector <91323245+TheH4ckingCollector@users.noreply.github.com>
2022-07-17 14:13:58 +0300

    Update file

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -7,15 +7,24 @@ public class Exceptions {
     }
 
     public static int calc(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter a number");
+        int x = getInput();
+        int y = getInput();
         try{
-            int data = s.nextInt();
-            s.close();
-            return data;
-        }catch (InputMismatchException e){
-            s.close();
-            return 0;
+            return x / y;
+        }catch(ArithmeticException e){
+            throw new ArithmeticException("You tried to divide by zero... ");
+        }
+    }
+
+    public static int getInput(){
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter a number: ");
+            try{
+                return scanner.nextInt();
+            } catch (InputMismatchException e){
+                System.out.println("Enter a valid number...");
+            }
         }
     }
 }
